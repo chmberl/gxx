b164f7a6dcceb6621ad6d3e0461d9bdc7d8744e5
chmberl <chmberl@gmail.com>
2015-11-08 01:21:43 +0800

    index Authorization

--- gxx/fancy/handlers/main.py
+++ gxx/fancy/handlers/main.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
+import tornado.web
 from .base import BaseHandler
 
 
 class IndexHandler(BaseHandler):
 
+    @tornado.web.authenticated
     def get(self):
         self.render("index.html", error=None)
